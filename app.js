06fb87281a513dacc706a58f11cd62176b6b0b75
mohitnegi724 <mohitnegi724@gmail.com>
2020-03-13 06:47:17 +0530

    Update data with pug

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.engine('pug', require('pug').__express)
+app.set('view engine',"pug")
+app.set('views', 'views')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
